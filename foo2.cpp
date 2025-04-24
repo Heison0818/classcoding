2cbd0caf781f137c1e831e2e67baf0e2874c863a
Heison0818 <jhu7@mail.sfsu.edu>
2025-04-23 22:53:56 -0700

    12123213

--- foo2.cpp
+++ foo2.cpp
@@ -1,12 +1,64 @@
 #include <iostream>
+#include <stdio.h>
 
-void sum(int N, int A[], int Y[]) {
-	for (int i = 0; i < N; i++) {
-    	int row_sum = 0;
-    	int base = i * N; // row offset
-    	for (int j = 0; j < N; j++) {
-        	row_sum += A[base + j];
-    	}
-    	Y[i] = row_sum;
-	}
+void
+sum_rows(int N, int A[], int Y[])
+{
+   
+  for (int i = 0; i < N; i++) {
+    int row_sum = 0;
+    int base = i * N;
+    for(int j = 0; j < N; j++) {
+        row_sum += A[base +j];
+    }
+    Y[i] = row_sum;
+  }
+}
+
+int main(int ac, char*av[])
+{
+//   int N=(1<<10);
+   int N=5;
+   int A[N*N];
+   int y[N];
+
+   // initialize A
+   for (int i=0;i<N*N;i++)
+      A[i] = i%N;
+
+   // initialize Y
+   for (int i=0; i<N; i++)
+      y[i] = -1 * i;
+
+
+   // print out the A array
+   printf(" Contents of the A[] array: \n");
+   for (int indx=0,i=0;i<N;i++)
+   {
+      for (int j=0;j<N;j++)
+         printf(" %d ", A[indx++]);
+      printf("\n");
+   }
+
+   // print out the y vector
+   printf(" Contents of the y[] vector: \n");
+   for (int indx=0,i=0;i<N;i++)
+   {
+      printf(" %d ", y[indx++]);
+   }
+   printf("\n");
+
+   sum_rows(N, A, y);
+
+   // now do verification check
+   int t=y[0], err=0, i;
+   for (i=1; err == 0 && i<N ; i++)
+      if (t != y[i])
+      {
+         err=1;
+         printf(" correctness check fails at i=%d\n",i);
+      }
+
+   if (err==0)
+      std::cout << "Correctness check succeeds " << std::endl;
 }
