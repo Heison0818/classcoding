d5a902e19b9b6a185f9ee0fa12a0374c24eb905e
Heison0818 <jhu7@mail.sfsu.edu>
2025-04-23 21:10:49 -0700

    12312

--- sum_array.cpp
+++ sum_array.cpp
@@ -12,7 +12,19 @@ sum_array(int N, int A[])
    // and return that sum to the caller
 
    // Put your code here, return the correct result
-   return 0;
+
+   int sum(int N, int A[]) {
+
+      int total = 0;
+      for(int i = 0; i<N; i++) {
+         total += A[i];
+      }
+      return total;
+   }
+
+
+
+
 }
 
 
