842583f97b33768b6830cedc09fd8826380bc250
Jingxi Hu <70980939+Heison0818@users.noreply.github.com>
2025-04-23 23:15:18 -0700

    Create dgemve_v.cpp

---
+++ dgemve_v.cpp
@@ -0,0 +1,18 @@
+const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
+
+/*
+ * This routine performs a dgemv operation
+ * Y :=  A * X + Y
+ * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
+ * On exit, A and X maintain their input values.
+ */
+void my_dgemv(int n, double* A, double* x, double* y) {
+   // insert your code here: implementation of vectorized vector-matrix multiply
+    for (int row = 0; row < n; row++) {
+        double temp = 0.0;
+        int base = row * n;
+        for (int col = 0; row < n; row++) {
+            temp += A[base + col] * x[col];
+        }
+        y[row] += temp;
+    }
